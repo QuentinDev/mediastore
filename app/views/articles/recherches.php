9f84d96177dfc2635657514a06f6171f806fc72b
web <web@cti-peda.cti.lan>
2016-06-14 15:19:23 +0200

    Ajout Habilage des données

--- app/views/articles/recherches.php
+++ app/views/articles/recherches.php
@@ -6,27 +6,4 @@
     <?php endforeach; ?>
 </div>
 
-<table class="u-full-width">
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Sex</th>
-        <th>Location</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Dave Gamache</td>
-        <td>26</td>
-        <td>Male</td>
-        <td>San Francisco</td>
-    </tr>
-    <tr>
-        <td>Dwayne Johnson</td>
-        <td>42</td>
-        <td>Male</td>
-        <td>Hayward</td>
-    </tr>
-    </tbody>
-</table>
+
