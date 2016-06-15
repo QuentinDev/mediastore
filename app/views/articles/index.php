4bf6303888703339f9000c90c5d05cf9fdf17d07
Mickaël VILLERS <mickael.villers@epitech.eu>
2016-06-15 09:02:29 +0200

    hofix url

--- app/views/articles/index.php
+++ app/views/articles/index.php
@@ -24,7 +24,7 @@
                         <?= $article->prix ?><i class="euro icon"></i>
                     </div>
                     <div class="type">
-                        <a href="#<?= $article->type ?>">type: <?= $article->type->name ?></a>
+                        <a href="#<?= $article->type->name ?>">type: <?= $article->type->name ?></a>
                     </div>
                 </div>
             </div>
