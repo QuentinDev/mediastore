82cdcebab255175a76ed60a862fe7264e9b65c3f
Mickaël VILLERS <mickael.villers@epitech.eu>
2016-06-15 11:03:12 +0200

    ajout des contolller du cart

--- app/controllers/PanierController.php
+++ app/controllers/PanierController.php
@@ -20,14 +20,24 @@ class PanierController extends BaseController
     }
     
     public function add($id) {
-         Redirect::prev();
+        $cart = new Cart();
+        $cart->addProduct((int)$id, (int)1, true);
+        Redirect::prev();
     }
     
     public function remove($id) {
-
+        $cart = new Cart();
+        $cart->removeProduct((int)$id);
+        Redirect::prev();
     }
 
     public function update($id) {
-
+        if (!isset($_POST['quantity']) || $_POST['quantity'] < 1) {
+            Redirect::prev();
+            return;
+        }
+        $cart = new Cart();
+        $cart->addProduct((int)$id, (int)$_POST['quantity'], false);
+        Redirect::prev();
     }
 }
