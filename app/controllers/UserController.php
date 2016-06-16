3605e80c41f66852c36a2a6146fdc6eb9a1392c7
Quentin BETTOUM <quentin.bettoum@epitech.eu>
2016-06-16 17:02:50 +0200

    no message

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -35,16 +35,6 @@ class UserController extends BaseController
                 Auth::setFlash("Une erreur est survenue, veuillez réessayer", "error");
             }
         }
-
-        // if(isset($_POST['login']) && isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['adresse']) && isset($_POST['cp']) && isset($_POST['tel'])) {
-        //     if(Auth::register($_POST['login'],$_POST['nom'],$_POST['prenom'],$_POST['password'],$_POST['email'],$_POST['adresse'],$_POST['cp'],$_POST['tel'])) {
-        //         if(Auth::auth($_POST['login'], $_POST['password'])) {
-        //             Redirect::url('ArticlesController@nouveautes');
-        //         }
-        //     }else{
-        //         Auth::setFlash("Une erreur est survenue, veuillez réessayer ultérieurement", "error");
-        //     }
-        // }
         echo $this->render('user/register.php');
     }
 
